3dc6a4040d18439691bafb6ec1ef05fe864343e1
elliebrainscape4-lang <elliebrainscape4@gmail.com>
2026-07-11 21:53:56 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,11 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
 import {
-  getFirestore
+    getFirestore,
+    collection,
+    getDocs
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB4yWqjP97NWHJu5FBBABG5U28pHk2rXWM",
   authDomain: "catchat-a5e92.firebaseapp.com",
@@ -18,15 +15,22 @@ const firebaseConfig = {
   appId: "1:856769501386:web:f902dc2ea472f9af54bc33"
 };
 
-// Initialise Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
-const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Make them available everywhere
-window.auth = auth;
 window.db = db;
 
-console.log("✅ Firebase connected!");
+async function testConnection() {
+
+    const querySnapshot = await getDocs(collection(db, "posts"));
+
+    querySnapshot.forEach((doc) => {
+
+        console.log(doc.id, doc.data());
+
+    });
+
+}
+
+testConnection();
