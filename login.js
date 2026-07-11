485f2cc7958ab685fc4168831dfcef911ad43a44
elliebrainscape4-lang <elliebrainscape4@gmail.com>
2026-07-11 22:02:04 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,50 @@
+import { auth } from "./firebase.js";
 
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const message = document.getElementById("message");
+
+document.getElementById("signupBtn").addEventListener("click", async () => {
+
+    try {
+
+        await createUserWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        );
+
+        message.textContent = "✅ Account created successfully!";
+
+    } catch (err) {
+
+        message.textContent = err.message;
+
+    }
+
+});
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email.value,
+            password.value
+        );
+
+        message.textContent = "✅ Logged in successfully!";
+
+    } catch (err) {
+
+        message.textContent = err.message;
+
+    }
+
+});
