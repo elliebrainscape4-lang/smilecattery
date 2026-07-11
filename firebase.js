80ca6a4e1c2fd671299d3466b3af66567b3690aa
elliebrainscape4-lang <elliebrainscape4@gmail.com>
2026-07-11 22:00:33 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,36 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 
 import {
-    getFirestore,
-    collection,
-    getDocs
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import {
+    getFirestore
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4yWqjP97NWHJu5FBBABG5U28pHk2rXWM",
-  authDomain: "catchat-a5e92.firebaseapp.com",
-  projectId: "catchat-a5e92",
-  storageBucket: "catchat-a5e92.firebasestorage.app",
-  messagingSenderId: "856769501386",
-  appId: "1:856769501386:web:f902dc2ea472f9af54bc33"
+    apiKey: "AIzaSyB4yWqjP97NWHJu5FBBABG5U28pHk2rXWM",
+    authDomain: "catchat-a5e92.firebaseapp.com",
+    projectId: "catchat-a5e92",
+    storageBucket: "catchat-a5e92.firebasestorage.app",
+    messagingSenderId: "856769501386",
+    appId: "1:856769501386:web:f902dc2ea472f9af54bc33"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
-
-window.db = db;
-
-async function testConnection() {
-
-    const querySnapshot = await getDocs(collection(db, "posts"));
-
-    querySnapshot.forEach((doc) => {
-
-        console.log(doc.id, doc.data());
-
-    });
-
-}
-
-testConnection();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
